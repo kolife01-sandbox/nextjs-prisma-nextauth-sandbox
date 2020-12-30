a3a94da1c11f7df8ff04fd1f3c84b89b44b71156
Kohei Tanaka <kohei69@gmail.com>
2020-12-30 11:14:26 +0900

    feat : add github

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -18,13 +18,10 @@ const options = {
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
-    //　プロバイダーは何個でも指定できる。
-    // https://next-auth.js.org/getting-started/introduction で一覧がみれる
-    // 例：
-    // Providers.Twitter({
-    //   clientId: process.env.TWITTER_CLIENT_ID,
-    //   clientSecret: process.env.TWITTER_CLIENT_SECRET,
-    // }),
+    Providers.GitHub({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET
+    }),
   ],
   adapter: Adapters.Prisma.Adapter({ prisma }),
   callbacks: {
